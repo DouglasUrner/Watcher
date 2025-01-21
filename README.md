e27198d82012421a47de8661308e90f87408475c
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2025-01-21 15:07:40 -0800

    Latest data: Tue Jan 21 23:07:39 UTC 2025

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Watcher
- Monitor a webpage for changes.
+
+Monitor a webpage for changes.
+
+The use case is to be able to monitor a set of webpages for interesting changes -- in particular, I'm interested in watching the HR pages of US corporations and school districts during the 2nd Trump administration.
+
+Original idea from [Gaelan](https://github.com/Gaelan)
+
+[Git scraping: track changes over time by scraping to a Git repository](https://simonwillison.net/2020/Oct/9/git-scraping/)
+
+## Goals
+
+Track & analyze polices and changes in policies.
+
+Get alerts to policy changes.
+
+## Install
+
+You'll need:
+* curl
+* [prettier] (which requires nodejs)
+* git
+* [HTML tidy](http://html-tidy.org)
+
+## Usage
---
+++ watcher.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+for url in $(cat urls.txt); do
+    echo '>>>' $url
+    name=$(echo $url | sed s./._.g)
+    path=out/$name
+    curl -m 60 -A "GaelanSteeleBot/1.0; (+https://gaelan.me)" $url > $path
+    if ! prettier --write --parser html $path; then
+        tidy -mi --force-output true -ashtml --drop-empty-elements no --drop-empty-paras no --fix-style-tags no --join-styles no --merge-emphasis no --tidy-mark no $path
+        prettier --write --parser html $path
+    fi
+
+    if [[ -f process/$name ]]; then
+        cp $path tmp
+        process/$name <tmp >$path
+        rm tmp
+
+        prettier --write --parser html $path
+    fi
+done
+
+# https://simonwillison.net/2020/Oct/9/git-scraping/
+git add -A
+timestamp=$(date -u)
+git commit -m "Latest data: ${timestamp}" || exit 0
+
+curl -s \
+    --form-string "t=Letting Update" \
+    --form-string "m=$(git diff --stat=999 'HEAD^1..HEAD')" \
+    --form-string k=$(cat ~/.config/pushsafer_key) \
+    https://www.pushsafer.com/api
+
+git push origin main
