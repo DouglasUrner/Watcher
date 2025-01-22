ac05d7a35bbd4cb08a77a51d976fc6de80056704
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2025-01-21 17:31:30 -0800

    Latest data: Wed Jan 22 01:31:30 UTC 2025

--- watcher.sh
+++ watcher.sh
@@ -4,10 +4,12 @@ for url in $(cat urls.txt); do
     echo '>>>' $url
     #name=$(echo $url | sed s./._.g)
     #path=out/$name
-    dir=$(./pathogen.sh $url)
+    dir=out/$(./pathogen.sh $url)
     name=$(date -Iseconds)
-    path=$dir/$timestamp
+    path=$dir/$name
     mkdir -p $dir
+    #echo $path
+    #exit
     curl -m 60 -A "GaelanSteeleBot/1.0; (+https://gaelan.me)" $url > $path
     if ! prettier --write --parser html $path; then
         tidy -mi --force-output true -ashtml --drop-empty-elements no --drop-empty-paras no --fix-style-tags no --join-styles no --merge-emphasis no --tidy-mark no $path
