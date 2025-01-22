ba6a321bac994d9b9e0d6da3cf7b00ccc2618bd0
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2025-01-21 18:54:09 -0800

    Latest data: Wed Jan 22 02:54:09 UTC 2025

--- watcher.sh
+++ watcher.sh
@@ -1,16 +1,17 @@
 #! /bin/sh
 
+# Watch a websites for changes by downloading pages and archiving them
+# in a git repository. Push notification of changes.
+
 for url in $(cat urls.txt); do
     echo '>>>' $url
-    #name=$(echo $url | sed s./._.g)
-    #path=out/$name
+ 
     dir=out/$(./pathogen.sh $url)
     name=$(date -Iseconds)
     path=$dir/$name
     mkdir -p $dir
-    #echo $path
-    #exit
-    curl -m 60 -A "GaelanSteeleBot/1.0; (+https://gaelan.me)" $url > $path
+
+    curl -m 60 -A "Watcher/0.1; (watcher@fastmail.org)" $url > $path
     if ! prettier --write --parser html $path; then
         tidy -mi --force-output true -ashtml --drop-empty-elements no --drop-empty-paras no --fix-style-tags no --join-styles no --merge-emphasis no --tidy-mark no $path
         prettier --write --parser html $path
@@ -31,7 +32,7 @@ timestamp=$(date -u)
 git commit -m "Latest data: ${timestamp}" || exit 0
 
 curl -s \
-    --form-string "t=Letting Update" \
+    --form-string "t=$(grep '^Name:' ./sites-group.txt | sed s.NAME:\ ..)" \
     --form-string "m=$(git diff --stat=999 'HEAD^1..HEAD')" \
     --form-string k=$(cat ~/.config/pushsafer_key) \
     https://www.pushsafer.com/api
